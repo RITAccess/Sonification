1066fd4942b5b20b1b5b3d6d07b203566e5487b9
Ethan Jurman <ehj2229@g.rit.edu>
2014-11-25 15:07:31 -0500

    draws marker and different data points on keypresses

--- src/com/sonification/graph/Graph.java
+++ src/com/sonification/graph/Graph.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.GeneralPath;
 import java.io.File;
 import java.util.Random;
@@ -21,13 +23,15 @@ public class Graph extends JPanel {
 
 	private static final int SIZE_WIDTH = 800;
 	private static final int SIZE_HEIGHT = 400;
-	private static final int BORDER = 25;
+	private static final int GRID_SIZE = 20;
 	private GeneralPath path = new GeneralPath();
-	private GeneralPath[] curves; 
+	private GeneralPath[] curves;
+	private int curveDrawn = 0;
 	private Double[] smallest;
 	private Double[] largest;
 	private Double multiplyVar = 1.0;
 	private Color[] colors;
+	private int position = 0;
 	
 	public Graph(String filepath){
 		this(new File(filepath));
@@ -67,23 +71,62 @@ public class Graph extends JPanel {
 			// Avoid rows with just String data
 			int j = 0;
 			while (!isDouble(data.get(i)[j])){ j++; }
-			p.moveTo(0 + BORDER, SIZE_HEIGHT - (multiplyVar * (Double.parseDouble(data.get(i)[j]) - smallest[i])) + BORDER);
+			p.moveTo(0 + GRID_SIZE, SIZE_HEIGHT - (multiplyVar * (Double.parseDouble(data.get(i)[j]) - smallest[i])) + GRID_SIZE);
 			System.out.println(p.getCurrentPoint());
 			while (j < data.get(i).length) {
 				if (isDouble(data.get(i)[j])){
 					double v = SIZE_HEIGHT - (multiplyVar * (Double.parseDouble(data.get(i)[j]) - smallest[i]));
-					p.lineTo(j*(SIZE_WIDTH / data.get(i).length) + BORDER, v + BORDER);
+					p.lineTo(j*(SIZE_WIDTH / data.get(i).length) + GRID_SIZE, v + GRID_SIZE);
 				}
 				j++;
 			}
 			curves[i] = p;
 			colors[i] = new Color(rnd.nextFloat(),rnd.nextFloat(),rnd.nextFloat());
+			
 		}
+	}
+	
+	/**
+	 * set the position of the marker
+	 * @param pos - int value measuring position
+	 */
+	public void setPosition(int pos){
+		this.position = pos;
+	}
+	
+	/**
+	 * get the position of the marker
+	 * @return pos - int value measuring position
+	 */
+	public int getPosition(){
+		return this.position;
+	}
+	
+	/**
+	 * set the curve being drawn (if 0, all curves are drawn)
+	 * @param curve
+	 */
+	public void setCurveDrawn(int curve){
+		this.curveDrawn = curve;
+		if ((this.curveDrawn >= curves.length) || (this.curveDrawn < 0)){
+			this.curveDrawn = 0;
+		}
+		if (curve == -1){
+			this.curveDrawn = curves.length - 1;
+		}
+	}
+	
+	/**
+	 * get the curve being drawn (if 0, all curves are drawn)
+	 * @return curveDrawn
+	 */
+	public int getCurveDrawn(){
+		return this.curveDrawn;
 	}
 
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension(SIZE_WIDTH + 50, SIZE_HEIGHT + 50);
+		return new Dimension(SIZE_WIDTH + GRID_SIZE, SIZE_HEIGHT + GRID_SIZE * 2);
 	}
 
 	@Override
@@ -96,26 +139,38 @@ public class Graph extends JPanel {
 		int w = this.getWidth();
 		int h = this.getHeight();
 
+
+		/*
+		 * Drawing current position
+		 */
+		path.reset();
+		path.moveTo(position * GRID_SIZE + GRID_SIZE, 0);
+		path.lineTo(position * GRID_SIZE + GRID_SIZE, SIZE_HEIGHT * 2); // make sure it covers the entire area
+		g2d.setStroke(new BasicStroke(15));
+		g2d.setColor(Color.LIGHT_GRAY);
+		g2d.draw(path);
+		
 		/*
 		 * Drawing grid
 		 */
 		path.reset();
-		for (double t = 0; t < w * 2; t = t + 20) {
+		for (double t = GRID_SIZE; t < w * 2; t = t + GRID_SIZE *2) {
 			path.moveTo(t, 0);
 			path.lineTo(t, h);
 		}
-		for (double t = 0; t < w * 2; t = t + 20) {
+		for (double t = GRID_SIZE; t < w * 2; t = t + GRID_SIZE *2) {
 			path.moveTo(w, t);
 			path.lineTo(0, t);
 		}
 		g2d.setColor(Color.gray);
+		g2d.setStroke(new BasicStroke(1));
 		g2d.draw(path);
 		
 		/*
 		 * Drawing curves
 		 */
 		g2d.setStroke(new BasicStroke(3, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-		for (int i = 0; i < curves.length; i++){
+		for (int i = curveDrawn; i < (curveDrawn == 0 ? curves.length : curveDrawn + 1); i++){
 			if (curves[i] != null){
 				// TODO make sure colors aren't similar / hurt
 				g2d.setColor(colors[i]);
@@ -136,8 +191,51 @@ public class Graph extends JPanel {
 	public static void main(String[] args) {
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.add(new Graph("/Users/Student/Downloads/download.csv"));
+		Graph g = new Graph("/Users/Student/Downloads/download.csv");
+		f.add(g);
 		f.pack();
 		f.setVisible(true);
+		f.addKeyListener(new KeyListener(){
+
+			private Graph g;
+			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				// 39 is right, 37 is left
+				if (arg0.getKeyCode() == 39){
+					g.setPosition(g.getPosition() + 1);
+					g.repaint();
+				}
+
+				if (arg0.getKeyCode() == 37){
+					g.setPosition(g.getPosition() - 1);
+					g.repaint();
+				}
+				
+				// 38 is up, 40 is down
+				if (arg0.getKeyCode() == 38){
+					g.setCurveDrawn(g.getCurveDrawn() + 1);
+					g.repaint();
+				}
+
+				if (arg0.getKeyCode() == 40){
+					g.setCurveDrawn(g.getCurveDrawn() - 1);
+					g.repaint();
+				}
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {}
+			
+			public KeyListener setGraph(Graph g){
+				this.g = g;
+				return this;
+			}
+			
+		}.setGraph(g));
 	}
 }
